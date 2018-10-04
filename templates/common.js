36367813c5c8b99a218322d7a77cf3f6ce4cb9a2
shifan <1475583665@qq.com>
2018-10-04 15:08:22 +0800

    增加find模块 表格中显示文件icon

--- templates/common.js
+++ templates/common.js
@@ -1,23 +1,31 @@
-function setCookie(c_name,value,expiredays)
-{
-var exdate=new Date()
-exdate.setDate(exdate.getDate()+expiredays)
-document.cookie=c_name+ "=" +escape(value)+
-((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
-}
+//写cookies 
+function setCookie(name,value,days) 
+{ 
+    if(null == days) days = 7; 
+    var exp = new Date(); 
+    exp.setTime(exp.getTime() + days*24*60*60*1000); 
+    var haha = name + "="+ escape (value) + ";expires=" + exp.toGMTString(); 
+    this.document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString(); 
+} 
 
-function getCookie(c_name)
-{
-if (document.cookie.length>0)
-  {
-  c_start=document.cookie.indexOf(c_name + "=")
-  if (c_start!=-1)
-    {
-    c_start=c_start + c_name.length+1
-    c_end=document.cookie.indexOf(";",c_start)
-    if (c_end==-1) c_end=document.cookie.length
-    return unescape(document.cookie.substring(c_start,c_end))
-    }
-  }
-return ""
-}
+//读取cookies 
+function getCookie(name) 
+{ 
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+ 
+    if(arr=document.cookie.match(reg))
+ 
+        return unescape(arr[2]); 
+    else 
+        return null; 
+} 
+
+//删除cookies 
+function delCookie(name) 
+{ 
+    var exp = new Date(); 
+    exp.setTime(exp.getTime() - 1); 
+    var cval=getCookie(name); 
+    if(cval!=null) 
+        document.cookie= name + "="+cval+";expires="+exp.toGMTString(); 
+} 
